7e62a792de0b96a852bed7821cddf96a25f74650
PCS <astro@pcschneider.eu>
2021-03-03 11:52:26 +0100

    numbers...

---
+++ checks/check_numbers.py
@@ -0,0 +1,52 @@
+from eroML.tile import file4
+from astropy.io import fits as pyfits
+import numpy as np
+
+fn = file4("major", cconfig="eFEDS_EDR3.ini")
+print(fn)
+ff = pyfits.open(fn)
+fd = ff[1].data
+
+gi = np.where(fd["NN"] == 1)[0]
+print("Found nearest neighbour to ",len(gi))
+mm = np.unique(fd["original_srcID"])
+mi = np.array([str("ML%05i" % int(x[2:])) for x in mm])
+print("   ",len(mm))
+print(mi)
+print()
+
+#fn = '../ero_data/eFEDS_c001_hs.fits'
+fn = "../ero_data/efeds_c001_V3_main_HamStar_internal.fits"
+
+ff = pyfits.open(fn)
+fd = ff[1].data
+li = np.array([str("ML%05i" % int(x)) for x in fd["ero_ID"]])
+try:
+    ll = np.unique(fd["ero_NAME"])
+    print("unique ero_name: ",len(ll))
+    print(type(mi), type(ll))
+    a = mi.astype("<U7")
+    b = li
+    print(a.dtype, b.dtype)
+    dd = np.setdiff1d(a, b)
+    #for j in b:
+        #if j not in a: print("not in b: ",j, len(j), "<",j,">") 
+    print(dd, len(dd))
+except:
+    pass
+
+print(len(fd))
+ee = ["fd[\"RADEC_ERR\"] > 0", "fd[\"DET_LIKE_0\"]>6", "fd[\"EXT_LIKE\"]<6"]
+a = ""
+for e in ee:
+    print(e)
+    gi = np.where(eval(e))[0]
+    print(len(gi))
+    print()
+
+a = " & ".join(["("+x+")" for x in ee])
+print(a)
+gi = np.where(eval(a))[0]
+print(len(gi))
+    
+    
